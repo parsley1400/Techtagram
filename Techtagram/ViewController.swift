9f0dc054c144a0ea55ce59967e07a3cf27a427c4
parsley1400 <topthecap@gmail.com>
2020-09-12 22:08:20 +0900

    Initial Commit

---
+++ Techtagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Techtagram
+//
+//  Created by risako takeya on 2020/09/12.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
